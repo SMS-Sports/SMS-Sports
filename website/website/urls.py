95093448daedd6aa0e18cadff7e7d2ff46c4ff78
Mohamed Moussa <mmmoussa@uwaterloo.ca>
2015-01-31 15:46:53 -0500

    Updated django files

--- website/website/urls.py
+++ website/website/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from testsite.home import views
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'website.views.home', name='home'),
+    url(r'^$', 'home.views.index', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^subscribe/', include('subscribe.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
